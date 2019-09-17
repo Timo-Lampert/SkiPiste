4eb0929c113c6f909587a68cbbfb3e8d150c0325
M01POOL16 <m01pool16@chg-meridian.com>
2019-09-17 14:13:51 +0200

    Updated version

--- SkiPiste/Program.cs
+++ SkiPiste/Program.cs
@@ -6,7 +6,27 @@ namespace SkiPiste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" 1.Talabfahrt 5,3KM\n 2.Sesselbahn 2,8KM\n 3.Schlepplift 1,3KM\n 4.Tellerlift 0,5KM\n 5.Gondelbahn 4,7KM \n ----------------------------------------");
+            double[] lift = { 5.3, 2.8, 1.3, 0.5, 3.7 };
+
+            double km = 0;
+            String inp = Console.ReadLine();
+            while(!inp.Equals("q"))
+            {
+                if (inp.Length == 1 && Char.IsDigit(Char.Parse(inp)) && Int32.Parse(inp) <= 5)
+                {
+
+                    int inpnum = Int32.Parse(inp) - 1;
+                    Console.WriteLine("Kilometer of the route " + (inpnum + 1) + ": " + lift[inpnum] + " km");
+                    km = km + lift[inpnum];
+                    Console.WriteLine("Your Journey is " + km + " kilometers long!");
+                }
+
+              inp = Console.ReadLine();
+                
+            
+
+            }
         }
     }
 }
